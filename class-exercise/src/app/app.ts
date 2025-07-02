3bd70a1182fd8eae884b2e397415a59b4e6f3123
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-07-02 17:27:40 +0300

    exercise finished subscriptions

--- class-exercise/src/app/app.ts
+++ class-exercise/src/app/app.ts
@@ -3,10 +3,11 @@ import { UserListComponents } from './user-list.components/user-list.components'
 import { UserService } from './user.service';
 import { IUser } from './interfaces/user';
 import { CommonModule } from '@angular/common';
+import { TimeComponent } from './time.component/time.component';
 
 @Component({
     selector: 'app-root',
-    imports: [UserListComponents, CommonModule],
+    imports: [UserListComponents, CommonModule, TimeComponent],
     templateUrl: './app.html',
     styleUrls: ['./app.css']
 })
@@ -20,7 +21,17 @@ export class App implements OnInit {
 
     loadUsers(search?: string): void {
         this.users = undefined;
-        this.userService.loadUsers(search).subscribe(users => this.users = users);
+        this.userService.loadUsers(search).subscribe(
+            users => this.users = users, // next fn
+            error => console.error(error), // error fn
+            () => console.log('load users stream completed') // cmpleted fn
+        );
+
+        /* this.userService.loadUsers(search).subscribe({
+            next: () => {},
+            error: () => {},
+            complete: () => {}
+        }); */
     }
 
     reloadButtonHandler() {
--- class-exercise/src/app/time.component/time.component.ts
+++ class-exercise/src/app/time.component/time.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, map, startWith, Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-time.component',
+  selector: 'app-time',
   imports: [],
   templateUrl: './time.component.html',
   styleUrl: './time.component.css'
 })
-export class TimeComponent {
+export class implements OnDestroy {
 
+    timeStreamSubscription: Subscription;
+
+    time$ = interval(1000).pipe(startWith(''), map(()=> new Date()));
+
+    timeValue!: Date;
+
+    constructor() {
+        this.timeStreamSubscription = this.time$.subscribe(timeValue => this.timeValue = timeValue);
+    }
+
+    ngOnDestroy(): void {
+        this.timeStreamSubscription.unsubscribe();
+    }
 }
